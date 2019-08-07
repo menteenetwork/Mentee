b013ba729e68b23a92de64d20f84f8857fa44bca
Commoneffort <aneta.karbowiak@gmail.com>
2019-08-07 18:03:19 +0200

    Update GroupChannel.js

--- src/GroupChannel.js
+++ src/GroupChannel.js
@@ -13,10 +13,16 @@ return(
 	
  		<h2>Group name:</h2><h3>Anna's Graphic design classes</h3>
  		<div>
+ 		{/* Here should appear the webcam boxes and chat messages from https://tox.chat 
+ 		
+ 		*/}
  		<img className="img-respons" src={"../../assets/images/icon-user-avatar.jpg"} alt="mentor"/>
  		<img className="img-respons" src={"../../assets/images/icon-user-avatar.jpg"} alt="mentee"/>
  		</div>
- 		<div class="chat.field"></div>
+ 		
+ 		{/* The icons should connect to the specific components: Members, CreateGroup, FileTransfer, MyNetworkMentees  
+ 		
+ 		*/}
  		<div>
  		<img className="img-responsive" src={"../../assets/images/icon-user.png"} alt="add mentee" a href="/members" />
 		<Route exact path="/members" component={Members}/>
